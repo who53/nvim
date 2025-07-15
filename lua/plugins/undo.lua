8e3a0f2c54e3403bdf7521643c3c32c6a63cf28e
Deepak Meena <who53@disroot.org>
2025-07-15 19:35:17 +0530

    Revert "undotree: no need to press enter"
    This reverts commit b4f5b0e5b702436f74331b0e196b81705960d21e.

--- lua/plugins/undo.lua
+++ lua/plugins/undo.lua
@@ -5,61 +5,16 @@ local M = {
 
 M.config = function()
     
-    local undotree = require("undotree")
-
-    undotree.setup({
-        float_diff = true,
-        layout = "left_bottom",
-        position = "left",
-        ignore_filetype = { 'undotree', 'undotreeDiff', 'qf', 'TelescopePrompt', 'spectre_panel', 'tsplayground' },
-        window = {
-            winblend = 30,
-        },
-        keymaps = {
-            ['j'] = "move_next",
-            ['k'] = "move_prev",
-            ['gj'] = "move2parent",
-            ['J'] = "move_change_next",
-            ['K'] = "move_change_prev",
-            ['<cr>'] = "action_enter",  
-            ['p'] = "enter_diffbuf",
-            ['q'] = "quit",
-        },
-    })
-
-    local is_moving = false
-
-    vim.api.nvim_create_autocmd("CursorMoved", {
-        pattern = "*",  
-        callback = function()
-            
-            local ft = vim.bo.filetype
-            if ft == "undotree" or ft == "undotreeDiff" then
-                
-                if is_moving then
-                    
-                    is_moving = false
-
-                    
-                    vim.api.nvim_win_call(0, function()
-                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true), 'n', true)
-                    end)
-                end
-            end
-        end,
-    })
+    require("undotree").setup()
     
     local mappings = {
-        { 'n', '<leader>u', function() undotree.toggle() end },
-        { 'n', 'j', function() is_moving = true; vim.cmd('normal! j') end },  
-        { 'n', 'k', function() is_moving = true; vim.cmd('normal! k') end },  
+        { 'n', '<leader>u', function() require('undotree').toggle() end }
     }
-
+    
     local opts = { noremap = true, silent = true }
-
+    
     for _, mapping in ipairs(mappings) do
         vim.keymap.set(mapping[1], mapping[2], mapping[3], opts)
     end
 end
-
 return M
