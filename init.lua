abc43a3b7be199aa8c8c18d57140bc70423712e3
Deepak Meena <who53@disroot.org>
2025-07-15 16:57:55 +0530

    nvim: drop nvterm
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- init.lua
+++ init.lua
@@ -12,7 +12,43 @@ vim.opt.undolevels = 300
 vim.opt.termguicolors = true
 vim.opt.syntax = "on"
 
+vim.keymap.set("n", "j", "gj", { noremap = true })
+vim.keymap.set("n", "k", "gk", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>f", ":%normal = GG<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>[", ":bnext<CR>", { noremap = true, silent = true })  
-vim.api.nvim_set_keymap("n", "<leader>]", ":bprevious<CR>", { noremap = true, silent = true })  
 vim.api.nvim_set_keymap("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
+
+local function skip_term(direction)
+  local cur = vim.api.nvim_get_current_buf()
+  repeat
+    vim.cmd(direction)
+  until vim.bo.buftype ~= "terminal" or vim.api.nvim_get_current_buf() == cur
+end
+
+vim.keymap.set("n", "<leader>]", function() skip_term("bnext") end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>[", function() skip_term("bprevious") end, { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype == "terminal" then
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end
+  end
+})
+
+vim.keymap.set("n", "<leader>t", function()
+    vim.cmd.vnew()
+    vim.cmd.term("fish")
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 7)
+    vim.cmd("stopinsert")
+end, { noremap = true, silent = true })
+
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
+for _, key in ipairs({ 'h', 'j', 'k', 'l' }) do
+  vim.keymap.set('n', '<leader>' .. key .. '', function()
+    vim.cmd('wincmd ' .. key)
+    vim.cmd('stopinsert')
+  end, { noremap = true })
+end
--- lua/plugins/bufline.lua
+++ lua/plugins/bufline.lua
@@ -1,9 +1,15 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        require("bufferline").setup()
-    end,
+  "akinsho/bufferline.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("bufferline").setup {
+      options = {
+        custom_filter = function(bufnr)
+          return vim.bo[bufnr].buftype ~= "terminal"
+        end,
+      },
+    }
+  end,
 }
