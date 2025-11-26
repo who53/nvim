f02bcfaa87cbe2eb57dfa5f2d06707b7dd23e4fe
Deepak Meena <who53@disroot.org>
2025-11-26 14:47:59 +0530

    plugins/lspconfig: fix
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,23 +6,23 @@ local M = {
 }
 
 M.config = function()
-  local lspconfig = require('lspconfig')
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-  
-  lspconfig.clangd.setup({
+  vim.lsp.config('clangd', {
     cmd = { "clangd", "--background-index" },
     filetypes = { "c", "cpp", "objc", "objcpp" },
     capabilities = capabilities,
+
     on_attach = function(client, bufnr)
       local bufopts = { noremap=true, silent=true, buffer=bufnr }
-      
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
       vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     end,
   })
+
+  vim.lsp.enable('clangd')
 end
 
 return M
