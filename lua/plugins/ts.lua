f643cdeaff05cb8a2ebdc8f4fc0898cddd6c63d7
Deepak Meena <who53@disroot.org>
2025-12-17 13:40:43 +0530

    plugins/ts : fix
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- lua/plugins/ts.lua
+++ lua/plugins/ts.lua
@@ -1,20 +1,7 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate"
 }
-
-M.config = function()
-    require("nvim-treesitter.configs").setup{
-        ensure_installed = "",
-        auto_install = true,
-        highlight = {
-            enable = true,  
-            additional_vim_regex_highlighting = false,
-        },
-        indent = {
-            enable = true,
-        },
-    }
-end
 
 return M
