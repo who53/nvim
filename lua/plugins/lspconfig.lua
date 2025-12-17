981dbb11bccb0f6ff50484c5d5b5d5f3e3a8bfa6
Deepak Meena <who53@disroot.org>
2025-12-17 13:52:38 +0530

    plugins/lspconfig: add qmlls
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -8,9 +8,11 @@ local M = {
 M.config = function()
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+  vim.lsp.enable('clangd')
+
   vim.lsp.config('clangd', {
     cmd = { "clangd", "--background-index" },
-    filetypes = { "c", "cpp", "objc", "objcpp" },
+    filetypes = { "c", "cpp" },
     capabilities = capabilities,
 
     on_attach = function(client, bufnr)
@@ -22,7 +24,22 @@ M.config = function()
     end,
   })
 
-  vim.lsp.enable('clangd')
+  vim.lsp.enable('qmlls')
+
+  vim.lsp.config('qmlls', {
+    cmd = { "qmlls6", "-E" },
+    filetypes = { "qml", "qmljs" },
+    capabilities = capabilities,
+
+    on_attach = function(client, bufnr)
+      local bufopts = { noremap=true, silent=true, buffer=bufnr }
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    end,
+  })
+
 end
 
 return M
