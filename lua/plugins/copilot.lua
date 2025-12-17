2725620784c99f312a70f1e65f3c26e50e3f93bb
Deepak Meena <who53@disroot.org>
2025-12-17 13:41:05 +0530

    plugins/copilot: use official plugin
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,14 +1,11 @@
 return {
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-    },
-    -- See Commands section for default commands if you want to lazy load on them
+    "github/copilot.vim",
+    event = "VeryLazy",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.api.nvim_set_keymap("i", "<C-.>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
   },
 }
