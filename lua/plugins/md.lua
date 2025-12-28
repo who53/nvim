2c51229835c30e4594f56113fc37846be12cfc31
Deepak Meena <who53@disroot.org>
2025-12-28 21:54:25 +0530

    l
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- lua/plugins/md.lua
+++ lua/plugins/md.lua
@@ -1,4 +1,3 @@
-
 return {
     "MeanderingProgrammer/render-markdown.nvim",
     enabled = true,
@@ -33,59 +32,4 @@ return {
         -- vim.cmd(string.format([[highlight Headline5Fg guifg=%s gui=bold]], colors.color5_bg))
         -- vim.cmd(string.format([[highlight Headline6Fg guifg=%s gui=bold]], colors.color6_bg))
     end,
-    opts = {
-        heading = {
-            sign = false,
-            icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
-            backgrounds = {
-                "Headline1Bg",
-                "Headline2Bg",
-                "Headline3Bg",
-                "Headline4Bg",
-                "Headline5Bg",
-                "Headline6Bg",
-            },
-            foregrounds = {
-                "Headline1Fg",
-                "Headline2Fg",
-                "Headline3Fg",
-                "Headline4Fg",
-                "Headline5Fg",
-                "Headline6Fg",
-            },
-        },
-        code = {
-            sign = false,
-            width = "block",
-            right_pad = 1,
-        },
-        bullet = {
-            -- Turn on / off list bullet rendering
-            enabled = true,
-        },
-        -- checkbox = {
-        --     -- Turn on / off checkbox state rendering
-        --     enabled = true,
-        --     -- Determines how icons fill the available space:
-        --     --  inline:  underlying text is concealed resulting in a left aligned icon
-        --     --  overlay: result is left padded with spaces to hide any additional text
-        --     position = "inline",
-        --     unchecked = {
-        --         -- Replaces '[ ]' of 'task_list_marker_unchecked'
-        --         icon = "   󰄱 ",
-        --         -- Highlight for the unchecked icon
-        --         highlight = "RenderMarkdownUnchecked",
-        --         -- Highlight for item associated with unchecked checkbox
-        --         scope_highlight = nil,
-        --     },
-        --     checked = {
-        --         -- Replaces '[x]' of 'task_list_marker_checked'
-        --         icon = "   󰱒 ",
-        --         -- Highlight for the checked icon
-        --         highlight = "RenderMarkdownChecked",
-        --         -- Highlight for item associated with checked checkbox
-        --         scope_highlight = nil,
-        --     },
-        -- },
-    },
 }
