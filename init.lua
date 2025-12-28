3d127a24e4e3cfe2eb4e868279593e10a6fb838e
Deepak Meena <who53@disroot.org>
2025-12-28 21:54:39 +0530

    make term toggable
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- init.lua
+++ init.lua
@@ -17,32 +17,42 @@ vim.keymap.set("n", "k", "gk", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>f", ":%normal = GG<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
 
-local function skip_term(direction)
-  local cur = vim.api.nvim_get_current_buf()
-  repeat
-    vim.cmd(direction)
-  until vim.bo.buftype ~= "terminal" or vim.api.nvim_get_current_buf() == cur
-end
-
 vim.keymap.set("n", "<leader>]", function() skip_term("bnext") end, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>[", function() skip_term("bprevious") end, { noremap = true, silent = true })
 
-vim.api.nvim_create_autocmd("QuitPre", {
-  callback = function()
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-      if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype == "terminal" then
-        vim.api.nvim_buf_delete(buf, { force = true })
-      end
-    end
-  end
-})
+local term_buf = nil
+local term_win = nil
 
-vim.keymap.set("n", "<leader>t", function()
-    vim.cmd.vnew()
-    vim.cmd.term("fish")
-    vim.cmd.wincmd("J")
-    vim.api.nvim_win_set_height(0, 7)
-    vim.cmd("stopinsert")
+vim.keymap.set("n", "<leader>i", function()
+  if term_win and vim.api.nvim_win_is_valid(term_win) then
+    vim.api.nvim_win_hide(term_win)
+    term_win = nil
+    return
+  end
+
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  local opts = {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  }
+
+  if not term_buf or not vim.api.nvim_buf_is_valid(term_buf) then
+    term_buf = vim.api.nvim_create_buf(false, true)
+  end
+  term_win = vim.api.nvim_open_win(term_buf, true, opts)
+  if vim.bo[term_buf].buftype ~= "terminal" then
+    vim.fn.termopen("fish")
+  end
+  vim.cmd("startinsert")
 end, { noremap = true, silent = true })
 
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
